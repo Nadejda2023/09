a21de6d095e06417ef457725186eb09455eb2a00
Nadejda2023 <126894056+Nadejda2023@users.noreply.github.com>
2023-10-08 23:12:18 +0500

    3

--- src/middlewares/middleware_rateLimit.ts
+++ src/middlewares/middleware_rateLimit.ts
@@ -66,7 +66,7 @@ const connections: rateLimitDBModel[] = []
      });
      console.log(count)
 
-     if (count + 1 > maxRequests) {
+     if (count  > maxRequests) {
         return res.sendStatus(429); // Отправка статуса "Слишком много запросов" (429), если лимит превышен
      }
       await rateLimitCollection.insertOne({ IP: IP, URL: URL, date: date });
