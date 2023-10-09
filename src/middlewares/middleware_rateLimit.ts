afa5732804808f7b8be240836dd72880b31df62d
Nadejda2023 <126894056+Nadejda2023@users.noreply.github.com>
2023-10-09 17:33:08 +0500

    3

--- src/middlewares/middleware_rateLimit.ts
+++ src/middlewares/middleware_rateLimit.ts
@@ -54,7 +54,7 @@ const connections: rateLimitDBModel[] = []
  } */
  export async function customRateLimit(req: Request, res: Response, next: NextFunction) {
   const IP = req.ip;
-  const URL = req.baseUrl || req.originalUrl;
+  const URL = req.originalUrl;
   const date = new Date();
 
   try {
@@ -62,11 +62,11 @@ const connections: rateLimitDBModel[] = []
      const count = await rateLimitCollection.countDocuments({
         IP: IP,
         URL: URL,
-        date: { $gte: (new Date(+date - interval)) },
+        date: { $gte: (new Date(Date.now() - interval)) },
      });
      console.log(count)
 
-     if ((count + 1) > maxRequests) {
+     if (count  >= maxRequests) {
         return res.sendStatus(429); // Отправка статуса "Слишком много запросов" (429), если лимит превышен
      }
       await rateLimitCollection.insertOne({ IP: IP, URL: URL, date: date });
